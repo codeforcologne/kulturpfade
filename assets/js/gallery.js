c58d5e5b4d842156e4a2688b5d790011e84b2fbf
Wolfram Eberius <eberius@gmail.com>
2025-06-16 10:39:51 +0200

    gallery

---
+++ assets/js/gallery.js
@@ -0,0 +1,34 @@
+let galleryItems = [];
+let galleryLightbox;
+
+fetch("./locales/frankenberg/de/gallery.json")
+  .then(response => response.json())
+  .then(data => {
+    galleryItems = data.map(item => ({
+      href: item.href,
+      type: 'image',
+      title: item.title
+    }));
+
+    // Initialisiere die Lightbox einmal
+    galleryLightbox = GLightbox({
+      elements: galleryItems,
+      touchNavigation: true,
+      loop: true
+    });
+  });
+
+document.getElementById("gallery-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (galleryLightbox) {
+    galleryLightbox.open(); // Öffnen ist jetzt synchron!
+  }
+});
+
+// Bilder aus Konfigurationsdatei laden
+document.getElementById("gallery-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  if (galleryLightbox) {
+    galleryLightbox.open(); // Öffnen ist jetzt synchron!
+  }
+});
