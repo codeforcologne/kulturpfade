0c220722c49825a8f1d4762decd01d4e1495160d
Wolfram Eberius <eberius@gmail.com>
2025-07-02 22:06:11 +0200

    korbach

--- assets/js/app.js
+++ assets/js/app.js
@@ -136,9 +136,10 @@ function syncSidebar() {
         const propsnr = (layer.feature.properties.point === "p") ? layer.feature.properties.nr : " "
         $("#feature-list tbody")
           .append('<tr class="feature-row" id="'
-            + L.stamp(layer) + '" lat="' + layer.getLatLng().lat + '" lng="' + layer.getLatLng().lng
+            + L.stamp(layer) + '" lat="' + layer.getLatLng().lat
+            + '" lng="' + layer.getLatLng().lng
             + '"><td style="vertical-align: middle;text-align: right" class="feature-nr">'
-            + propsnr
+            + '&nbsp;'
             + '</td><td class="feature-name">'
             + layer.feature.properties.name
             + '</td><td style="vertical-align: middle;"><i class="fa fa-chevron-right pull-right"></i></td></tr>');
@@ -156,9 +157,27 @@ function syncSidebar() {
 }
 
 /* Basemap Layers */
-var osm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+var osm = L.tileLayer("https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+/* OpenTopoMap */
+var topo = L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: 'Kartendaten: &copy; OpenStreetMap-Mitwirkende, Kartendarstellung: &copy; OpenTopoMap (CC-BY-SA)'
+});
+
+/* CartoDB */
+var cartodb = L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
+  maxZoom: 19,
+  attribution: '&copy; OpenStreetMap-Mitwirkende & CARTO'
+});
+
+/* Wikimedia Maps */
+var wikimedia = L.tileLayer("https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: '&copy; OpenStreetMap-Mitwirkende, CC-BY-SA'
 });
 
 /* Overlay Layers */
@@ -239,19 +258,21 @@ var poiLayer = L.geoJson(null);
 
 var pois = L.geoJson(null, {
   pointToLayer: function (feature, latlng) {
-    // If style is 0, show empty string, else show nr
-    const iconHtml = (feature.properties.point === "o") ? "" : feature.properties.nr;
-    const iconSize = (feature.properties.point === "o") ? [20, 20] : [40, 30];
-    return L.marker(latlng, {
-      icon: L.divIcon({
-        className: 'red-tooltip',
-        html: '<div class="icon-container">' + iconHtml + '</div>',
-        iconSize: iconSize,
-        iconAnchor: [20, 10]
-      }),
-      title: feature.properties.nr,
-      riseOnHover: true
-    });
+      // If style is 0, show empty string, else show nr
+  //    const iconHtml = (feature.properties.point === "o") ? "" : feature.properties.nr;
+  //    const iconSize = (feature.properties.point === "o") ? [20, 20] : [40, 30];
+      const iconHtml = "";
+      const iconSize = [20, 20];
+      return L.marker(latlng, {
+        icon: L.divIcon({
+          className: 'red-tooltip',
+          html: '<div class="icon-container">' + iconHtml + '</div>',
+          iconSize: iconSize,
+          iconAnchor: [20, 10]
+        }),
+        title: feature.properties.nr,
+        riseOnHover: true
+      });
   },
   onEachFeature: function (feature, layer) {
     if (feature.properties) {
@@ -272,10 +293,7 @@ var pois = L.geoJson(null, {
 
       layer.on({
         click: function (e) {
-          const featureTitle =
-            (feature.properties.point === "p") | (feature.properties.point === "u") ?
-              feature.properties.nr + ' ' + feature.properties.name :
-              feature.properties.name;
+          const featureTitle = feature.properties.name;
           $("#feature-title").html(featureTitle);
           $("#feature-info").html(content);
           $("#featureModal").modal("show");
@@ -288,24 +306,18 @@ var pois = L.geoJson(null, {
         .append('<tr class="feature-row" id="'
         + L.stamp(layer) + '" lat="' + layer.getLatLng().lat + '" lng="' + layer.getLatLng().lng
         + '"><td style="vertical-align: middle;">'
-        + propsnr + '</td><td class="feature-name">'
+        + '&nbsp;'
+        + '</td><td class="feature-name">'
         + layer.feature.properties.name
         + '</td><td style="vertical-align: middle;"><i class="fa fa-chevron-right pull-right"></i></td></tr>');
 
-      var tooltipOptions;
-      if (feature.properties.point === "p" | feature.properties.point === "u") {
-        tooltipOptions = {
-            offset: [10, 5], // Offset des Tooltips
-            direction: 'auto', // Kein Pfeil, Tooltip wird zentriert auf dem Marker.
-            opacity: 0.8, // Opazität des Tooltips
-            interactive: false, // Tooltip soll nicht interaktiv sein
-       }} else {
-        tooltipOptions = {
-            offset: [10, 0], // Offset des Tooltips
+      var tooltipOptions = {
+            offset: [15, 0], // Offset des Tooltips
             direction: 'auto', // Kein Pfeil, Tooltip wird zentriert auf dem Marker.
             opacity: 0.7, // Opazität des Tooltips
-            interactive: false, // Tooltip soll nicht interaktiv sein
-       }};
+            interactive: false // Tooltip soll nicht interaktiv sein
+      };
+
       // bind tooltip Options to layer
       layer.bindTooltip('<div>'
         + layer.feature.properties.name
@@ -365,7 +377,7 @@ function loadPoiLayer() {
 map = L.map("map", {
   zoom: 14,
   center: [50.944511,6.849597],
-  layers: [osm, routes, markerClusters, highlight],
+  layers: [wikimedia, routes, markerClusters, highlight],
   zoomControl: false,
   attributionControl: false
 });
@@ -484,7 +496,10 @@ if (document.body.clientWidth <= 767) {
 }
 
 var baseLayers = {
-  "Street Map": osm
+  "Wikimedia": wikimedia,
+  "CartoDB": cartodb,
+  "OSM": osm,
+  "Topo": topo
 };
 
 var groupedOverlays = {
