1238b3294e6283328f2b4db6c569736a5a76d7f1
Wolfram Eberius <eberius@gmail.com>
2025-07-14 19:53:38 +0200

    korbach

--- assets/js/app.js
+++ assets/js/app.js
@@ -117,7 +117,7 @@ function clearHighlight() {
 
 function sidebarClick(id) {
   var layer = markerClusters.getLayer(id);
-  map.setView([layer.getLatLng().lat, layer.getLatLng().lng], 15);
+  map.setView([layer.getLatLng().lat, layer.getLatLng().lng], 20);
   layer.fire("click");
   /* Hide sidebar and go to the map on small screens */
   if (document.body.clientWidth <= 767) {
